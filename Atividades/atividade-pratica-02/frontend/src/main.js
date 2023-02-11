f5dd9039df64b571f70b31e8933d1ea481955308
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2023-02-11 15:19:43 -0300

    PComponent Home

--- Atividades/atividade-pratica-02/frontend/src/main.js
+++ Atividades/atividade-pratica-02/frontend/src/main.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import Vuelidate from 'vuelidate';
+import App from './App.vue';
+import router from './router';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/nprogress/nprogress.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');

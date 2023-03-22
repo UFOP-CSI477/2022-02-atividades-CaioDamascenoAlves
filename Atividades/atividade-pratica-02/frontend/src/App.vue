e37fa31010eb317ef5ad9947adcd2772e8d9b4ae
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-03-22 17:04:33 -0300

    Estruturando sidebar responsiva

--- Atividades/atividade-pratica-02/frontend/src/App.vue
+++ Atividades/atividade-pratica-02/frontend/src/App.vue
@@ -1,48 +1,121 @@
 <template>
-	<!--Container Main end-->
 	<div id="app">
-
-		<!-- INÍCIO NAVBAR -->
-		<b-navbar toggleable type="dark" variant="dark">
-			<b-navbar-brand href="#">Blood Donation
-				<router-link to="/createLocaisColeta" class="m-5">
-					<button class="btn btn-primary rounded-pill ">Doar Sangue</button>
-				</router-link>
-			</b-navbar-brand>
-
-
-			<div>
-				<b-dropdown text="Menu" variant="outline-light" class="m-2">
-					<b-dropdown-item href="#">
-						<a class="nav-link" @click="logOutUser">Log Out</a>
-					</b-dropdown-item>
-				</b-dropdown>
-
-			</div>
-		</b-navbar>
-		<!-- FIM NAVBAR -->
-		<div class="container-fluid" style="">
+		<div class="sidebar" :class="{ hidden: isMobile }">
+			<ul>
+				<li>
+					<router-link to="/home" class="btn text-dark">
+						<font-awesome-icon :icon="['fas', 'house']" />
+						Home
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/profile" class="btn text-dark">
+						<font-awesome-icon :icon="['fas', 'user']" />
+						Perfil
+					</router-link>
+				</li>
+				<li>
+					<router-link to="/createDonation" class="btn text-dark">
+						<font-awesome-icon :icon="['fas', 'droplet']" />
+						Doe Sangue
+					</router-link>
+				</li>
+				<li>
+					<button class="btn btn-danger text-dark" @click="logOutUser()">
+						<font-awesome-icon :icon="['fas', 'right-from-bracket']" />
+						Logout
+					</button>
+				</li>
+			</ul>
+		</div>
+		<div class="content">
 			<router-view></router-view>
 		</div>
 	</div>
 </template>
-
-<script>
-
-export default {
-	name: 'App',
-	methods: {
-		logOutUser() {
-			localStorage.removeItem('jwt');
-			this.$router.push('/');
-		},
-	},
-}
-</script>
   
-<style scoped>
-.navbar {
-	background: linear-gradient(to left, #fd1919, #f71e1e, #000);
+<style>
+.sidebar {
+	position: fixed;
+	top: 0;
+	left: -10px;
+	width: 120px;
+	height: 100%;
+	background: linear-gradient(to bottom, #fc5454 0%, #ef764e 33%, #eea084 66%, #6d6f50 99%);
+	border-radius: 5%;
+	transition: left 0.3s ease;
+}
+
+.sidebar ul {
+	list-style: none;
+	margin: 0;
+	padding: 0;
+}
+
+.sidebar li {
+	margin: 0;
+	padding: 0;
+}
+
+.sidebar a,
+.sidebar button {
+	display: block;
+	padding: 10px;
+	color: #fff;
+	text-decoration: none;
+	border: none;
+	background-color: transparent;
+	cursor: pointer;
+	width: 100%;
+}
+
+.sidebar a:hover {
+	background-color: transparent;}
+
+.sidebar button:hover {
+	background-color: 0;
+}
+
+.content {
+	margin-left: 120px;
+	padding: 20px;
+}
+
+/* Oculta a sidebar em telas menores */
+@media (max-width: 800px) {
+	.sidebar {
+		left: -200px;
+	}
+
+	.sidebar.hidden {
+		display: none;
+	}
+
+	.content {
+		margin-left: 0;
+	}
 }
 </style>
-
+  
+<script>
+export default {
+	data() {
+		return {
+			isMobile: false,
+		};
+	},
+	mounted() {
+		// Verifica se é uma tela móvel
+		if (window.innerWidth < 768) {
+			this.isMobile = true;
+		}
+	},
+	methods: {
+		logOutUser() {
+			localStorage.removeItem("jwt");
+			this.$router.push("/");
+		},
+	},
+};
+</script>
+  

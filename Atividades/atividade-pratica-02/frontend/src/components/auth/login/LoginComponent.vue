553b3fea7622dd32bd00e5b64035ce552932ae29
CaioDamascenoAlves <caio.damasceno@aluno.ufop.edu.br>
2023-03-22 17:06:13 -0300

    titulo formulario

--- Atividades/atividade-pratica-02/frontend/src/components/auth/login/LoginComponent.vue
+++ Atividades/atividade-pratica-02/frontend/src/components/auth/login/LoginComponent.vue
@@ -2,9 +2,10 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-6 offset-lg-3 col-sm-10 offset-sm-1">
+				<h2>Login</h2>
 				<form class="text-center border border-dark p-5"
 					style="margin-top: 70px; height: auto; padding-top: 100px !important"
-					v-on:submit.prevent="loginSubmitUserForm()">
+					v-on:submit.prevent="loginSubmitUserForm()"> 
 					<!--INÍCIO BLOCO: E-mail-->
 					<div class="form-group">
 						<input type="text" id="email" name="email" class="form-control mb-5"
